9049f26ee6746425ddcbcd62f5dec3da4f3770dc
NikyDev <66072452+NikyDev@users.noreply.github.com>
2021-02-27 10:21:49 +0100

    primo commit

---
+++ Venturi.Nicolas.4J.contagi/Models/ContagiAlGiorno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Venturi.Nicolas._4J.contagi.Models
+{
+    class ContagiAlGiorno
+    {
+
+        string data;
+        int progressivo;
+        int contagi;
+        
+        
+        public string Data { get => data; set => data=value; }
+        public int Progressivo { get=> progressivo; set=>progressivo=value; }
+        public int Contagi { get=>contagi; set=>contagi=value; }
+
+        public override string ToString()
+        {
+            return "progressivo =" + Progressivo + " , " + Data + " contagi =" + Contagi;
+        }
+    }
+}
---
+++ Venturi.Nicolas.4J.contagi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Venturi.Nicolas._4J.contagi.Models;
+using System.IO;
+
+namespace Venturi.Nicolas._4J.contagi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int progStart,progEnd;
+            string strProgstart="", StrProgEnd;
+            StreamReader file = new StreamReader("ContagiRimini.json");
+            string json = file.ReadToEnd();
+            
+           ContagiAlGiorno[] covid= JsonConvert.DeserializeObject<ContagiAlGiorno[]>(json);
+            file.Close();
+            Console.WriteLine("inserisci il progressivo da cui vuoi iniziare a vedere i contagi");
+            strProgstart= Console.ReadLine();
+            progStart = Convert.ToInt32(strProgstart);
+            Console.WriteLine("inserisci il progressivo finale");
+            StrProgEnd = Console.ReadLine();
+            progEnd = Convert.ToInt32(StrProgEnd);
+            int i = progStart - 1;
+            while(i<=progEnd-1)
+            {
+                Console.WriteLine("progressivo=" + (i + 1) + " contagi= " + Convert.ToString(covid[i].Contagi));
+                i++;
+            }
+                
+            
+            Console.ReadLine();
+        }
+    }
+}
